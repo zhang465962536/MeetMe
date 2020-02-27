70b1881a3ffc846b843e8c18803669b2ec652917
zhangxiaohei <1013984329@qq.com>
2020-02-27 08:35:10 +0800

    MediaPlayer媒体播放

--- app/src/main/java/com/example/meetme/MainActivity.java
+++ app/src/main/java/com/example/meetme/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.meetme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.AssetFileDescriptor;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.view.View;
 import com.example.framework.base.BaseUIActivity;
 import com.example.framework.utils.LogUtils;
 import com.example.framework.utils.TimeUtils;
+import com.example.manager.MediaPlayerManager;
 
 public class MainActivity extends BaseUIActivity {
 
@@ -18,5 +20,15 @@ public class MainActivity extends BaseUIActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        MediaPlayerManager mediaPlayerManager = new MediaPlayerManager();
+        AssetFileDescriptor fileDescriptor = getResources().openRawResourceFd(R.raw.guide);
+        mediaPlayerManager.startPlay(fileDescriptor);
+
+        mediaPlayerManager.setOnProgressListener(new MediaPlayerManager.OnMusicProgressListener() {
+            @Override
+            public void OnProgress(int progress, int pos) {
+                LogUtils.e(pos + "%");
+            }
+        });
     }
 }
